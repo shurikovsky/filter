3b71253d6272a9fa458fc8714637a29cff79c094
Aleksandr Kryukov <AKryukov5@slb.ru>
2025-03-16 05:27:14 +0500

    add component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,66 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-const [state, setCount] = useState(0);
+//const [state, setCount] = useState(0);
 
 const Portfolio = {
-  filters,
-  selected,
-  projects
+  filters: ['All', 'Websites', "Flayers", "Business Cards"],
+  selected: 'All',
+  projects: [{
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
+    category: "Flayers"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
+    category: "Flayers"
+  }],
+  onSelectFilter: (filter) => {
+    this.selected = filter;
+  }
 }
 
  function Toolbar({
@@ -16,31 +70,38 @@ const Portfolio = {
    onSelectFilter
  }) {
   return (
-
+   <div class='filters'>
+     <div class='filter' onClick={onSelectFilter}>
+        {filters[0]}
+     </div>
+     <div class='filter' onClick={onSelectFilter}>
+        {filters[1]}
+     </div>
+     <div class='filter' onClick={onSelectFilter}>
+        {filters[2]}
+     </div>
+     <div class='filter' onClick={onSelectFilter}>
+        {filters[3]}
+     </div>
+   </div>
   )
 }
 
  function ProjectList({projects}) {
-
+   projects.forEach(element => {
+    return (
+      <div class='photo'>{element}</div>
+    )
+   });
  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='container'>
+      <Toolbar filters={Portfolio.filters}/>
+      <div class='photos'>
+        <ProjectList {... Portfolio.projects}/>
+      </div>
     </div>
   );
 }
